fbcb5c7f903634afe4a41ac0458b3be67f985f85
Sandeep <sandeepmandal@gmail.com>
2026-01-14 03:17:32 +0530

    Print All Element of Matrix in spiral Manner

---
+++ Java_program/Two_2D_Array/SpiralMatrix.java
@@ -0,0 +1,50 @@
+package Java_program.Two_2D_Array;
+
+public class SpiralMatrix {
+    public static void main(String[] args) {
+        int matrix[][]={{1,2,3,4},
+                        {5,6,7,8},
+                        {9,10,11,12},
+                        {13,14,15,16}};
+        PrintSpiral(matrix);
+    }
+
+    private static void PrintSpiral(int[][] matrix) {
+        int startrow=0;
+        int startcol=0;
+        int endrow=matrix.length-1;
+        int endcol=matrix[0].length-1;
+
+        while(startrow<=endrow && startcol<=endcol){
+            //upper row
+            for(int j=startcol;j<=endcol;j++){
+                System.out.print(matrix[startrow][j]+" ");
+            }
+
+            //Right side col
+            for(int i=startrow+1;i<=endrow;i++){
+                System.out.print(matrix[i][endcol]+" ");
+            }
+            //bottom row
+            for(int j=endcol-1;j>=startcol;j--){
+                if(startrow==endrow){
+                    break;
+                }
+                System.out.print(matrix[endrow][j]+" ");
+            }
+            //Left col
+            for(int i=endrow-1;i>=startrow+1;i--){
+                if(startcol==endcol){
+                    break;
+                }
+                System.out.print(matrix[i][startcol]+" ");
+            }
+            startcol++;
+            startrow++;
+            endrow--;
+            endcol--;
+        }
+
+    }
+}
+
