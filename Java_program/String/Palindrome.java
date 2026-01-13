0a21781d80df1b8129b8a40d569b8b021a158185
Sandeep <sandeepmandal@gmail.com>
2026-01-14 03:12:45 +0530

    Check String is Palindrome or Not

---
+++ Java_program/String/Palindrome.java
@@ -0,0 +1,18 @@
+package Java_program.String;
+
+public class Palindrome {
+    public static boolean isPalindrome(String str){
+        for(int i=0;i<str.length();i++){
+            int n=str.length();
+            if(str.charAt(i)!=str.charAt(n-1-i)){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        String str="madam";
+        System.out.print(isPalindrome(str));
+    }
+}
+
