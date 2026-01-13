af53e2f09ddabb4cdce6447ff29ca8ee16d53879
Sandeep <sandeepmandal@gmail.com>
2026-01-14 03:16:12 +0530

    Diagonal sum of 2D Matrix

---
+++ Java_program/Two_2D_Array/DiagonalSum.java
@@ -0,0 +1,23 @@
+package Java_program.Two_2D_Array;
+
+public class DiagonalSum {
+    public static void main(String[] args) {
+        int matrix[][]={{1,2,3,4},
+                {5,6,7,8},
+                {9,10,11,12},
+                {13,14,15,16}};
+        System.out.println(Sum(matrix));
+    }
+
+    private static int Sum(int[][] matrix) {
+        int sum=0;
+        for(int i=0;i< matrix.length;i++) {
+            sum += matrix[i][i];
+
+            if (i != matrix.length - 1 - i) {
+                sum += matrix[i][matrix.length - 1 - i];
+            }
+        }
+        return sum;
+}
+}
